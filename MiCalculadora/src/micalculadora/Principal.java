358c51c8978ef06d3e12488463a20a23d3a3e488
usuario <usuario@usuario-PC>
2018-02-13 22:14:59 -0500

    Calculadora con trigo

---
+++ MiCalculadora/src/micalculadora/Principal.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class Principal {
+    public static void main(String[] args){ 
+        
+        MiCalculadora miCalculadora;
+        miCalculadora=new MiCalculadora();
+                
+        
+        Trigonometrica trigo;
+        trigo=new Trigonometrica();
+        System.out.println("Seleccione el tipo de operación:");
+        System.out.println("1) suma");
+        System.out.println("2) resta");
+        System.out.println("3) seno");
+        System.out.println("4) coseno");
+        
+        Scanner myS;
+        myS = new Scanner(System.in);
+        switch (myS.nextInt()) {     
+            case 1:
+                System.out.println("Ingrese X:");
+                miCalculadora.setX(myS.nextDouble());
+                System.out.println("Ingrese Y:");
+                miCalculadora.setY(myS.nextDouble());
+                miCalculadora.sumar();
+                System.out.println(miCalculadora.getR());
+            case 2:
+                System.out.println("Ingrese X:");
+                miCalculadora.setX(myS.nextDouble());
+                System.out.println("Ingrese Y:");
+                miCalculadora.setY(myS.nextDouble());
+                miCalculadora.restar();
+                System.out.println(miCalculadora.getR());
+            case 3:
+                System.out.println("Ingrese X:");
+                miCalculadora.setX(myS.nextDouble());
+                trigo.seno();
+                System.out.println(trigo.getR());
+            case 4:
+                System.out.println("Ingrese X:");
+                miCalculadora.setX(myS.nextDouble());
+                trigo.coseno();
+                System.out.println(trigo.getR());
+        }      
+    }
+}
+           
+    
+        
+                
+    
+
+
+
---
+++ MiCalculadora/src/micalculadora/Trigonometrica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+import static java.lang.Math.*;
+
+public class Trigonometrica extends MiCalculadora{
+    
+    public void seno(){
+        setR(sin(x));
+    }
+    
+    public void coseno(){
+        setR(cos(x));
+    }
+    
+}
+
